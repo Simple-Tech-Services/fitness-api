5df3e88e9d5795baecd064beddcacd44802845d3
Carlos Flores <carlosfloresalex@gmail.com>
2022-10-30 23:43:37 -0400

    added basic route files for users and workouts

---
+++ controller/workouts.py
@@ -0,0 +1,5 @@
+class Workouts:
+    def __init__(self):
+        pass
+    def getWorkouts(self):
+        return "Getting all workouts"
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from controller.users import Users
+from controller.workouts import Workouts
 
 app = Flask(__name__)
 
+usersRoute = Users()
+
+
 @app.route("/")
 def hello_world():
-	return "<p>Welcome to our fitness app api!!!</p>"
+    return "welcome to our fitness app api"
+
+# Register User Routes
+
+# Register Workout Routes
