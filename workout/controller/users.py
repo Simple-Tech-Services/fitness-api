5e0be79230b4f0da3419283ae813f3999c51b7f3
Carlos Flores <carlosfloresalex@gmail.com>
2022-12-31 00:26:04 -0500

    updated get method for user route to return user data

--- workout/controller/users.py
+++ workout/controller/users.py
@@ -1,10 +1,10 @@
 from workout import app
 from workout import db
-from flask import request
+from flask import request, jsonify
 
 from workout.model.user import UserModel
 
-@app.route("/users", methods=['POST','DELETE', 'GET', 'PUT'])
+@app.route("api/users", methods=['POST','DELETE', 'GET', 'PUT'])
 def route_users():
     if(request.method == 'GET'):
         return 'got all users'
@@ -28,17 +28,28 @@ def route_users():
 
         return "user was created"
     
+    else:
+        return "This route does not exist"
+
+@app.route("api/user/<id>", methods=['POST','DELETE', 'GET', 'PUT'])
+def route_user_id(id):
+    if (request.method == "GET"):
+        data = {}
+
+        result = db.session.execute(db.select(UserModel).filter_by(id=id)).one()
+
+        for row in result:
+            data = {"username": row.username,
+                    "firstName": row.first_name,
+                    "lastName": row.last_name}
+
+        return jsonify(data)
+
     elif (request.method == 'DELETE'):
         return "deleting users"
 
     elif (request.method == 'PUT'):
         return "updates users"
+    
     else:
         return "This route does not exist"
-
-@app.route("/user/<id>", methods=['POST','DELETE', 'GET', 'PUT'])
-def route_user_id(id):
-    if (request.method == "GET"):
-        username = db.session.execute(db.select(UserModel.username).filter_by(id=id)).one()
-
-        return f'got user {username}'
