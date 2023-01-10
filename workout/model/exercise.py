20d9554d7489da62799602ba527eb9d998b5ce3b
Josiel Perez <josielp0218@gmail.com>
2023-01-09 23:40:49 -0500

    added foreignkey to exercise

--- workout/model/exercise.py
+++ workout/model/exercise.py
@@ -1,4 +1,6 @@
 from workout import db
-
+import sqlalchemy as sa
+from workout import WorkoutModel
 class ExerciseModel(db.Model):
-    pass
+    workout_id = sa.Column(sa.ForeignKey(WorkoutModel.id))
+    
