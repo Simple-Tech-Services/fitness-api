e1ee12d320e2aad053cdc8ceaf682f71bec51060
Josiel Perez <josielp0218@gmail.com>
2023-01-10 22:50:14 -0500

    added delete method for workout

--- workout/controller/workouts.py
+++ workout/controller/workouts.py
@@ -22,3 +22,24 @@ def route_workouts():
         
     else:
         return "This route does not exist"
+
+@app.route("/api/workout/<id>", methods=['POST', 'GET', 'DELETE', 'PUT'])
+def route_workout_id(id):
+
+    if (request.method == 'GET'):
+        return "getting a workout"
+
+    elif (request.method == 'DELETE'):
+        result = None
+
+        try:
+            result = db.session.execute(db.select(WorkoutModel).filter_by(id=id)).one()
+        
+        except:
+            return "workout was not found"
+        
+        for workout in result:
+            db.session.delete(workout)
+            db.session.commit()
+
+        return "deleting a workout"
