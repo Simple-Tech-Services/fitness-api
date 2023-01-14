f4ed6f1857f8e2766f620f9087925bdf49b506a2
Josiel Perez <josielp0218@gmail.com>
2023-01-14 00:14:27 -0500

    updated exercise models

--- workout/model/exercise.py
+++ workout/model/exercise.py
@@ -2,5 +2,9 @@ from workout import db
 import sqlalchemy as sa
 from workout import WorkoutModel
 class ExerciseModel(db.Model):
+    id = sa.Column(sa.Integer, primary_key=True)
     workout_id = sa.Column(sa.ForeignKey(WorkoutModel.id))
+    name = sa.Column(sa.String(25), nullable = False)
+    sets = sa.Column(sa.Integer, nullable = False)
+    reps = sa.Column(sa.Integer, nullable = False)
     
